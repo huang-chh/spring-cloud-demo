a541227cc8c57c8f453d198533522d53040eaf97
tiger <18341252812@163.com>
2020-06-28 23:52:00 +0800

    注册中心添加用户验证功能

---
+++ test-cloud-eureka/src/main/java/com/test/cloud/testcloudeureka/config/WebSecurityConfig.java
@@ -0,0 +1,37 @@
+package com.test.cloud.testcloudeureka.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @Date 2020/6/28
+ * @Author tiger
+ */
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    /**
+     * 高版本的丢弃了
+     *
+     * security:
+     *   basic:
+     *    enabled: true
+     *
+     * 配置，应该使用以下方式开启
+     *
+     * @param http
+     * @throws Exception
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/actuator/**").permitAll()
+                .anyRequest()
+                .authenticated().and().httpBasic();
+    }
+}
